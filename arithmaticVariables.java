a85246fcbd868a80a58d3e32c5297a0dbf7491de
ShivamRoyJavaDeveloper <shik78691@gmail.com>
2024-05-06 20:38:28 +0530

    Adding a code for arithmaticVariables in java

---
+++ arithmaticVariables.java
@@ -0,0 +1,22 @@
+package i02Basics;
+
+public class arithmaticVariables {
+    public static void main(String[]args){
+        int a;
+        int b;
+        a = 12;
+        b = 10;
+        float c = (a/b);
+        System.out.println(a*b);
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b); // 1.2
+        System.out.println(c);
+
+        // if int replaced by float it will show decimal value with accurate answer
+        float d,f;
+        d=12;
+        f=10;
+        System.out.println(d/f);
+    }
+}
