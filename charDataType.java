d618aa5ddceebfd705a7bd0a47ab7bcfcf99401f
ShivamRoyJavaDeveloper <shik78691@gmail.com>
2024-05-06 20:43:00 +0530

    Adding a code about CharactersTypesOfData in java

---
+++ charDataType.java
@@ -0,0 +1,7 @@
+package i02Basics;
+public class charDataType {
+    public static void main(String[]args){
+        System.out.println(8);   // it is a value.
+        System.out.println("8"); // it is  a text.
+    }
+}
