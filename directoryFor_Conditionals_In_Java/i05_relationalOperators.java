3ef596b09b511c7e7cb44deab32edc1c9cb60beb
ShivamRoyJavaDeveloper <shik78691@gmail.com>
2024-05-07 21:24:07 +0530

    Adding a code related to ralationalOperators in java

---
+++ directoryFor_Conditionals_In_Java/i05_relationalOperators.java
@@ -0,0 +1,13 @@
+package i03conditionals;
+
+public class i05_relationalOperators {
+    public static void main(String[] args) {
+        int a = 5;
+        int c = 5;
+
+        System.out.println(a==3);
+        System.out.println(a!=3);
+        System.out.println(a==c);
+        System.out.println(a&c);
+    }
+}
