33b4c83b2edb1fb42b0ea78bbe10279d7cc30739
ShivamRoyJavaDeveloper <shik78691@gmail.com>
2024-05-12 03:52:04 +0530

    Adding a loops For loop programme

---
+++ LoopsPart01/i01_Loop.java
@@ -0,0 +1,11 @@
+package i04LoopPart01;
+import java.util.Scanner;
+public class i01_Loop {
+    public static void main(String [] args){
+        Scanner sf = new Scanner(System.in);
+        int n = sf.nextInt();
+        for(int i=1; i<=n;  i++){
+            System.out.println("Hellow World");
+        }
+    }
+}
