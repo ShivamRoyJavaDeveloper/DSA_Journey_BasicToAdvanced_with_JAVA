c9c2a22b57140399fc7089f62cc077c3b14fcfd7
ShivamRoyJavaDeveloper <shik78691@gmail.com>
2024-05-17 23:29:36 +0530

    Adding a code to declare and Memory allocation for an Arrays

---
+++ Z_1_DimensionalArrays/i02ArryasMInMax.java
@@ -0,0 +1,16 @@
+package i08Arrays;
+import java.util.Scanner;
+public class i02ArryasMInMax {
+    public static void main(String[]args){
+        Scanner d=new Scanner(System.in);
+//        int n =d.nextInt();
+
+        int [] arr=new int [5];  //Declaration and Allocation
+        arr [0]=78;
+        arr [1]=8;
+        arr [2]=7;
+        arr [3]=9;
+        arr [4]=5;
+        System.out.println(arr[3]);
+    }
+}
