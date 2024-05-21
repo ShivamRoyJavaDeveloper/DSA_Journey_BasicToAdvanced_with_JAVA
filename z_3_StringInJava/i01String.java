3f78665fd4b8db78120a7c8be2b70098b6be7697
ShivamRoyJavaDeveloper <shik78691@gmail.com>
2024-05-21 23:59:01 +0530

    intro to String in java

---
+++ z_3_StringInJava/i01String.java
@@ -0,0 +1,11 @@
+package i10Sstring;
+import java.util.*;
+public class i01String {
+    public static void main(String[]args){
+        Scanner sc =new Scanner(System.in);
+        char a='k';  // only one character can ber used! otherwise it will give error!
+        String f="d   1 kjijkjfnjgkj}{{{/.,|\nd"; //more than one word and also can be give a space !
+        String k=sc.nextLine();
+        System.out.println("hello world!"+k);
+    }
+}
