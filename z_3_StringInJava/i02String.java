9146eefff21989c449439ea8862e48c12802196c
ShivamRoyJavaDeveloper <shik78691@gmail.com>
2024-05-22 00:00:01 +0530

    print indivisual character and a String

---
+++ z_3_StringInJava/i02String.java
@@ -0,0 +1,34 @@
+package i10Sstring;
+//import java.util.Scanner;
+public class i02String {
+    public static void main(String[]args){
+//        Scanner sc=new Scanner(System.in);
+         String str="I am a boy!";
+         int l= str.length();
+         char a=str.charAt(0);
+         char b=str.charAt(1);
+         char c=str.charAt(2);
+         char s=str.charAt(3);
+         char f=str.charAt(4);
+         char h=str.charAt(5);
+         char d=str.charAt(6);
+         char g=str.charAt(7);
+         char k=str.charAt(8);
+         char v=str.charAt(9);
+         char n=str.charAt(10);
+        System.out.println(l);
+        System.out.print(a);
+        System.out.print(b);
+        System.out.print(c);
+        System.out.print(s);
+        System.out.print(f);
+        System.out.print(h);
+        System.out.print(d);
+        System.out.print(g);
+        System.out.print(k);
+        System.out.print(v);
+        System.out.print(n);
+        System.out.print("\n");
+        System.out.println(str);
+    }
+}
