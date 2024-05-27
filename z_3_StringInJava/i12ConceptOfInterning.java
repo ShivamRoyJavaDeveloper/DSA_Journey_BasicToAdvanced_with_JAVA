2d26469ca503f6ac9a33928a0c88547324faa005
ShivamRoyJavaDeveloper <shik78691@gmail.com>
2024-05-27 23:31:44 +0530

    code interning is problem

---
+++ z_3_StringInJava/i12ConceptOfInterning.java
@@ -0,0 +1,10 @@
+package i10Sstring;
+public class i12ConceptOfInterning {
+    public static void main(String[]args){
+        //see the notes!
+        /// it is most important concept
+        String s="sjhghg";
+        char r=s.charAt(2);
+        System.out.println(s.charAt(2));
+    }
+}
