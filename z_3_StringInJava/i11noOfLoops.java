9397e508fb44cacb222705b961f21c942b91ef5d
ShivamRoyJavaDeveloper <shik78691@gmail.com>
2024-05-27 23:30:48 +0530

    code to no.Of Loops

---
+++ z_3_StringInJava/i11noOfLoops.java
@@ -0,0 +1,11 @@
+package i10Sstring;
+import java.util.Scanner;
+public class i11noOfLoops {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        long a=sc.nextLong();
+        String s=""+a;
+        int l=s.length();
+        System.out.println(l);
+    }
+}
