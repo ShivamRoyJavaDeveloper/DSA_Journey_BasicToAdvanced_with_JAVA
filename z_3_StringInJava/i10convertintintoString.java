99d84edaa920fea8c0c3b4806fdb450053df1e84
ShivamRoyJavaDeveloper <shik78691@gmail.com>
2024-05-27 23:29:51 +0530

    code to convert into String

---
+++ z_3_StringInJava/i10convertintintoString.java
@@ -0,0 +1,10 @@
+package i10Sstring;
+import java.util.Scanner;
+public class i10convertintintoString {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        String s=""+a;
+        System.out.println(s);
+    }
+}
