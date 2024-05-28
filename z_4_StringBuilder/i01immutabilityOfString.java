f9733062f4333578348b77b9276d7c674b10a26e
ShivamRoyJavaDeveloper <shik78691@gmail.com>
2024-05-28 15:05:03 +0530

    code to intro for String Builder in java

---
+++ z_4_StringBuilder/i01immutabilityOfString.java
@@ -0,0 +1,7 @@
+package i11StringBuilders;
+public class i01immutabilityOfString {
+    public static void main(String []args){
+        ///see notes
+        ///we can not change indivisual characters in String
+    }
+}
